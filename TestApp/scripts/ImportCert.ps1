713f51a02026e452ee83f50966bb8a7cc91ce4f5
Jacob Rahme <jacob.rahme@circleci.com>
2020-04-22 14:26:59 -0400

    test parameterized script

--- TestApp/scripts/ImportCert.ps1
+++ TestApp/scripts/ImportCert.ps1
@@ -1,4 +1,13 @@
-$cert = Import-PfxCertificate -FilePath C:\Users\circleci\project\TestApp\TestApp_TemporaryKey.pfx -Password (ConvertTo-SecureString -String "testpassword" -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My
-Export-Certificate -Cert $cert -File c:\cert.sst -Type SST
+param(
+    [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$false)]
+    [System.String]
+    $KeyPath,
+
+    [Parameter(Mandatory=$True, Position=1, ValueFromPipeline=$false)]
+    [System.String]
+    $KeyPass
+)
+$Cert = Import-PfxCertificate -FilePath $KeyPath -Password (ConvertTo-SecureString -String "$KeyPass" -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My
+Export-Certificate -Cert $Cert -File c:\Cert.sst -Type SST
 Import-Certificate -File c:\cert.sst  -CertStoreLocation Cert:\LocalMachine\Root 
 Import-Certificate -File c:\cert.sst -CertStoreLocation Cert:\CurrentUser\My 
